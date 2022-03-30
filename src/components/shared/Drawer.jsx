1386ccb4e060410eaa9de9418dac3d457b3ef785
Aaron Blum <blumaa@gmail.com>
2022-03-30 16:01:19 +0200

    minor adjustment

--- src/components/shared/Drawer.jsx
+++ src/components/shared/Drawer.jsx
@@ -8,7 +8,7 @@ const Drawer = ({ setIsDrawerOpen, setIsloading }) => {
       <Link to='/kinkytower-1'>KinkyTower1</Link>
       <Link to='/kinkytower-2'>KinkyTower2</Link>
       <Link to='/titmouse-flight'>Titmouse Flight</Link>
-      {/* <Link onClick={()=>setIsloading(true)} to='/kinkytowers-1'>Kinky Towers 1</Link> */}
+      <Link to='/kinkytowers-1'>Kinky Towers 1</Link>
       <Link to='/kinkytowers-1'>Kinky Towers 1</Link>
     </div>
   )
