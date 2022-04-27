2481ec9364c1e852790b232cf6e9021f78cef2b2
Aaron Blum <blumaa@gmail.com>
2022-04-27 15:27:02 +0200

    fixed bell for firefox

--- src/scenes/BubblingTowerAndMoon/BubblingTowerAndMoon.jsx
+++ src/scenes/BubblingTowerAndMoon/BubblingTowerAndMoon.jsx
@@ -6,16 +6,23 @@ import LittleTitmouse from '../../assets/stylesheets/animations/LittleTitmouse'
 import BellSound from '../../assets/sounds/bell-sound.mp3'
 import Bell from '../../assets/stylesheets/animations/Bell'
 
-const durationNum = 2 
+const durationNum = 2
 const bellVariants = {
-  visible: { opacity: 1, scale: [1.1, 0.9, 1.2, 0.8, 1.1, 0.9, 1], rotate: [20, -20, 0, 15, -15, 0, 10, -10, 0, 5, -5, 0, 2, -2, 0 ], transition: { duration: durationNum, ease: "easeOut" } },
+  visible: {
+    opacity: 1,
+    originX: .5,
+    originY: .5,
+    scale: [1.1, 0.9, 1.2, 0.8, 1.1, 0.9, 1],
+    rotate: [20, -20, 0, 15, -15, 0, 10, -10, 0, 5, -5, 0, 2, -2, 0],
+    transition: { duration: durationNum, ease: 'easeOut' },
+  },
   initial: { opacity: 1, scale: 1, transition: { duration: durationNum } },
-};
+}
 const BubblingTowerAndMoon = () => {
   const towerRef = React.useRef()
   const [reset, setReset] = React.useState(false)
-  const [ bellRing, setBellRing] = React.useState(false)
-  const bellControls = useAnimation();
+  const [bellRing, setBellRing] = React.useState(false)
+  const bellControls = useAnimation()
 
   let audio = new Audio(BellSound)
 
@@ -25,28 +32,29 @@ const BubblingTowerAndMoon = () => {
 
   React.useEffect(() => {
     console.log('reset', reset)
-  reset && setTimeout(() => {
-      setReset(false)
-    }, 5000);
+    reset &&
+      setTimeout(() => {
+        setReset(false)
+      }, 5000)
   }, [reset])
 
   React.useEffect(() => {
-    bellRing && bellControls.start("visible") && setBellRing(false)
-  }, [bellControls, bellRing]);
+    bellRing && bellControls.start('visible') && setBellRing(false)
+  }, [bellControls, bellRing])
 
   return (
     <div
       ref={towerRef}
       className='TowerScene'
       key='tower-scene'
-    // initial={{ opacity: 0 }}
-    // animate={{ opacity: 1 }}
-    // exit={{ opacity: 0 }}
-    // transition={{
-    //   delay: 0,
-    //   ease: 'easeOut',
-    //   duration: 2,
-    // }}
+      // initial={{ opacity: 0 }}
+      // animate={{ opacity: 1 }}
+      // exit={{ opacity: 0 }}
+      // transition={{
+      //   delay: 0,
+      //   ease: 'easeOut',
+      //   duration: 2,
+      // }}
     >
       <motion.div
         onClick={() => {
@@ -54,20 +62,20 @@ const BubblingTowerAndMoon = () => {
           start()
           setBellRing(true)
         }}
-        className="TowerScene__reset-wrapper"
+        className='TowerScene__reset-wrapper'
         whileHover={{ scale: 1.2 }}
-          initial="initial"
-          variants={bellVariants}
-          animate={bellControls}
+        initial='initial'
+        variants={bellVariants}
+        animate={bellControls}
         // whileTap={{ scale: [0.9, 1.1, 0.8, 1.2, 1] }}
       >
-          <Bell className="TowerScene__bell" />
+        <Bell className='TowerScene__bell' />
       </motion.div>
       <div className='TowerScene__beast'>
-        <LittleBeast towerRef={towerRef} reset={reset} isDraggable={"x"} />
+        <LittleBeast towerRef={towerRef} reset={reset} isDraggable={'x'} />
       </div>
       <div className='TowerScene__tit'>
-        <LittleTitmouse towerRef={towerRef} reset={reset} isDraggable={"y"} />
+        <LittleTitmouse towerRef={towerRef} reset={reset} isDraggable={'y'} />
       </div>
       <div className='TowerScene__three'>
         <KinkyTowers1 reset={reset} />
